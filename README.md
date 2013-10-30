a103b08a38a3b5ef1c0a360ae3ac8b81dce3ce27
keisatou <dummy>
2013-10-31 02:06:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+carton install
+carton exec perl -Ilib script/bbs-server
+```
+access http://127.0.0.1:5000/
---
+++ lib/BBS/Web/Dispatcher.pm
@@ -0,0 +1,34 @@
+package BBS::Web::Dispatcher;
+use strict;
+use warnings;
+use utf8;
+use Amon2::Web::Dispatcher::RouterBoom;
+
+any '/' => sub {
+  my ($c) = @_;
+
+  my @entries = $c->db->search(
+    entry => {
+    }, {
+      order_by => 'entry_id DESC',
+      limit    => 10,
+    }
+  );
+  return $c->render( "index.tx" => { entries => \@entries, } );
+};
+
+post '/post' => sub {
+  my ($c) = @_;
+
+  if (my $body = $c->req->param('body')) {
+    $c->db->insert(
+      entry => +{
+        body => $body,
+      }
+    );
+  }
+  return $c->redirect('/');
+};
+
+1;
+
